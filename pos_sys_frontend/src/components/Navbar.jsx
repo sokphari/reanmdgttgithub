382c2aa9bc43c770e057e7ef62314e4238f6ad3c
Sok Phari <sokphari@example.com>
2026-09-02 13:21:29 +0700

    fix code

--- pos_sys_frontend/src/components/Navbar.jsx
+++ pos_sys_frontend/src/components/Navbar.jsx
@@ -1,1 +1,14 @@
-raf
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul class="flex justify-center">
+                <li class="mx-4">Home</li>
+                <li class="mx-4">About</li>
+                <li class="mx-4">Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
